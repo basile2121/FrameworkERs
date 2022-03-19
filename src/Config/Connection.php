14c90e36a372b5ba71e78c6e3c9038ee7c9b5284
Basile2121 <basile.regnault@gmail.com>
2022-03-19 16:43:00 +0100

    [Projet] Ajout du mvc fais en cours de php

--- src/Config/Connection.php
+++ src/Config/Connection.php
@@ -1,64 +1,36 @@
 <?php
 
-/**
- * Database connection
- *
- *
- *
- * @author adapted from Benjamin Besse
- *
- * @link   http://fr3.php.net/manual/fr/book.pdo.php classe PDO
- */
-
 namespace App\Config;
 
-use PDO;
-use PDOException;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
 
-/**
- *
- * This class only make a PDO
- *
- */
 class Connection
 {
-    private PDO $pdoConnection;
+  public function init(): EntityManager
+  {
+    // On indique à Doctrine où aller chercher les entités
+    // pour les analyser et les mapper dans la base de données
+    $paths = [__DIR__ . "/../Entity"];
+    $isDevMode = ($_ENV['APP_ENV'] === 'dev');
 
-    /**
-     * Initialize connection
-     *
-     * @access public
-     */
-    public function __construct()
-    {
+    $dbParams = [
+      'driver'   => $_ENV['DB_DRIVER'],
+      'host'     => $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'],
+      'user'     => $_ENV['DB_USER'],
+      'password' => $_ENV['DB_PASSWORD'],
+      'dbname'   => $_ENV['DB_NAME'],
+    ];
 
-    }
+    $config = Setup::createAnnotationMetadataConfiguration(
+      $paths,
+      $isDevMode,
+      null,
+      null,
+      false
+    );
 
-    public function init()
-    {
-        try {
-            $this->pdoConnection = new PDO(
-                'mysql:host=' . $_ENV['DB_HOST']. '; dbname=' .  $_ENV['DB_NAME'] . '; charset=utf8',
-                $_ENV['DB_USER'],
-                $_ENV['DB_PASSWORD']
-            );
-            $this->pdoConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            // show errors in DEV environment
-            if ($_ENV['APP_ENV'] === 'dev') {
-                $this->pdoConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-
-        } catch (PDOException $e) {
-            echo '<div class="error">Error !: ' . $e->getMessage() . '</div>';
-        }
-    }
-
-    /**
-     * @return PDO $pdo
-     */
-    public function getPdoConnection(): PDO
-    {
-        return $this->pdoConnection;
-    }
+    // Un gestionnaire d'entités = paramètres de connexion + objet configuration
+    return EntityManager::create($dbParams, $config);
+  }
 }
