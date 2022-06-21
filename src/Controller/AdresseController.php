3a676332aa2767669b633369877315e900ffb95c
Basile2121 <basile.regnault@gmail.com>
2022-06-21 15:57:55 +0200

    [Maps] Debut de l'association de la map à l'adresse

--- src/Controller/AdresseController.php
+++ src/Controller/AdresseController.php
@@ -24,6 +24,7 @@ class AdresseController extends AbstractController
     public function adresses(AdressesRepository $adressesRepository)
     {
         $adresses = $adressesRepository->selectAll();
+        var_dump($this->geocodeAddress());
 
         echo $this->twig->render('admin/adresses/admin_adresse.html.twig', [
             'adresses' => $adresses,
@@ -147,5 +148,50 @@ class AdresseController extends AbstractController
         $id = intval($_POST['id']);
         $adressesRepository->delete($id);
         header('Location: /admin/adresses');
+    }
+
+    public static function geocodeAddress() {
+        $address = '151 avenue du pont trinquat 34070 Montpellier';
+        $apikey = 'AIzaSyBRsHEL7Jw33YmfNGNU_5F9WajSPaurcrs';
+        //valeurs vide par défaut
+        $data = array('address' => '', 'lat' => '', 'lng' => '', 'city' => '', 'department' => '', 'region' => '', 'country' => '', 'postal_code' => '');
+        //on formate l'adresse
+        $address = str_replace(" ", "+", $address);
+        var_dump($address);
+        //on fait l'appel à l'API google map pour géocoder cette adresse
+        $json = file_get_contents("https://maps.google.com/maps/api/geocode/json?key=" . $apikey . "&address=$address&sensor=false&region=fr");
+        $json = json_decode($json);
+        var_dump($json);
+        //on enregistre les résultats recherchés
+        if ($json->status == 'OK' && count($json->results) > 0) {
+            $res = $json->results[0];
+            //adresse complète et latitude/longitude
+            $data['address'] = $res->formatted_address;
+            $data['lat'] = $res->geometry->location->lat;
+            $data['lng'] = $res->geometry->location->lng;
+            foreach ($res->address_components as $component) {
+                //ville
+                if ($component->types[0] == 'locality') {
+                    $data['city'] = $component->long_name;
+                }
+                //départment
+                if ($component->types[0] == 'administrative_area_level_2') {
+                    $data['department'] = $component->long_name;
+                }
+                //région
+                if ($component->types[0] == 'administrative_area_level_1') {
+                    $data['region'] = $component->long_name;
+                }
+                //pays
+                if ($component->types[0] == 'country') {
+                    $data['country'] = $component->long_name;
+                }
+                //code postal
+                if ($component->types[0] == 'postal_code') {
+                    $data['postal_code'] = $component->long_name;
+                }
+            }
+        }
+        return $data;
     }
 }
